3df24103902413b9da2db156c9bfb073d3d02896
MIchael W <>
2020-09-22 15:31:07 +0200

    added some basic structure to javascript

--- uebung/js/myScript.js
+++ uebung/js/myScript.js
@@ -1,3 +1,31 @@
+// Input Fields
+var inputfieldFood = document.getElementById("inputfieldFood");
+var inputfieldSowing = document.getElementById("inputfieldSowing");
+var inputfieldAcreBuy = document.getElementById("inputfieldAcreBuy");
+var inputfieldAcreSell = document.getElementById("inputfieldAcreSell");
+// Buttons
+var buttonFood = document.getElementById("buttonFood");
+var buttonSowing = document.getElementById("buttonSowing");
+var buttonAcreBuy = document.getElementById("buttonBuyAcre");
+var buttonAcreSell = document.getElementById("buttonSellAcre");
+var buttonNextRound = document.getElementById("buttonNextRound");
+// Variables
+var year;
+var grains;
+var citizens;
+var acres;
+var acrePrice;
+
+function setupGame() {
+    year = 1;
+    grains = 6000;
+    citizens = 100;
+    acres = 400;
+    document.getElementById("year").innerHTML = year;
+    document.getElementById("grain").innerHTML = grains;
+    document.getElementById("citizen").innerHTML = citizens;
+    document.getElementById("acre").innerHTML = acres;
+}
 
 
 
@@ -17,11 +45,7 @@
 
 
 
-
-
-
-
-// regulates the tab
+// regulates the tabs
 function openInput(event, option) {
     // Declare all variables
     var i, tabcontent, tablinks;
@@ -41,4 +65,17 @@ function openInput(event, option) {
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(option).style.display = "block";
     event.currentTarget.className += " active";
-  } 
+}
+
+window.onload = setupGame;
+// Input Fields
+inputfieldFood.addEventListener("keyup", );
+inputfieldSowing.addEventListener("keyup", );
+inputfieldAcreBuy.addEventListener("keyup", );
+inputfieldAcreSell.addEventListener("keyup", );
+// Buttons
+buttonFood.addEventListener("click", );
+buttonSowing.addEventListener("click", );
+buttonAcreBuy.addEventListener("click", );
+buttonAcreSell.addEventListener("click", );
+buttonNextRound.addEventListener("click", );
